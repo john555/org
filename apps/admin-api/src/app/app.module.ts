18697cbe1799b31c216fb75e4482edda694809cd
JP <oyomthojohnpaul@gmail.com>
2024-07-27 12:01:37 +0300

    Remove unused code

--- apps/admin-api/src/app/app.module.ts
+++ apps/admin-api/src/app/app.module.ts
@@ -1,7 +1,4 @@
 import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
@@ -54,7 +51,7 @@ import { EventEmitterModule } from '@nestjs/event-emitter';
     StaffMemberModule,
     ShopModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
