ef6cd063cc04df400d27689592366b4b598d1de9
JP <oyomthojohnpaul@gmail.com>
2024-07-29 00:24:09 +0300

    Update files

--- scripts/generate-module/templates/template-module/template-module.dtos.ts
+++ scripts/generate-module/templates/template-module/template-module.dtos.ts
@@ -1,9 +1,18 @@
-import { InputType } from '@nestjs/graphql';
+import { Field, InputType } from '@nestjs/graphql';
+import { IsNotEmpty, IsOptional, IsUUID } from 'class-validator';
 
 @InputType()
-export class TemplateModuleCreateInput {}
+export class TemplateModuleCreateInput {
+  @Field({ nullable: true })
+  @IsUUID(undefined, { message: 'Invalid id' })
+  @IsOptional()
+  id?: string;
+}
 
 @InputType()
 export class TemplateModuleUpdateInput {
+  @Field()
+  @IsUUID(undefined, { message: 'Invalid id' })
+  @IsNotEmpty()
   id: string;
 }
--- scripts/generate-module/templates/template-module/template-module.entity.ts
+++ scripts/generate-module/templates/template-module/template-module.entity.ts
@@ -1,6 +1,25 @@
-import { ObjectType } from '@nestjs/graphql';
-import { Entity } from 'typeorm';
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+import {
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity({ name: 'template_modules' })
 @ObjectType({ description: 'templateModule' })
-export class TemplateModule {}
+export class TemplateModule {
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => ID)
+  id: string;
+
+  @UpdateDateColumn()
+  @Field(() => Date, {
+    description: 'Date the TemplateModule was last updated',
+  })
+  updatedAt: Date;
+
+  @CreateDateColumn()
+  @Field(() => Date, { description: 'Date the TemplateModule was created' })
+  createdAt: Date;
+}
